dca3e7171560fe3d876e3b10f1a8203dcde4441f
ANURAG PRAJAPATI <anuragprajapati02005@gmail.com>
2025-04-04 09:20:31 +0530

    change the package-lock.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
+import {  FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
